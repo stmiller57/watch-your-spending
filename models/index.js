8820fe73ea96407850eeecc5938c109cba6688f6
Reformed Scribe <stmiller57@gmail.com>
2020-07-03 11:37:42 -0400

    Updated service worker file to make application installable offline

--- models/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    Transaction: require("./models/transaction")
-};
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,75 @@
-console.log("Hello from service worker!")
+const cacheFiles = [
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    '/',
+    '/index.html',
+    '/index.js',
+    '/manifest.webmanifest',
+    '/styles.css',
+];
+
+const staticCache = "static-cache";
+const dataCache = "data-cache";
+
+// install
+self.addEventListener("install", function (evt) {
+    evt.waitUntil(
+        caches.open(staticCache).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(cacheFiles);
+        })
+    );
+
+    self.skipWaiting();
+});
+
+// activate
+self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== staticCache && key !== dataCache) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+// fetch
+self.addEventListener("fetch", function (evt) {
+    if (evt.request.url.includes("/api/")) {
+        evt.respondWith(
+            caches.open(dataCache).then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(evt.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+
+        return;
+    }
+
+    evt.respondWith(
+        caches.open(staticCache).then(cache => {
+            return cache.match(evt.request).then(response => {
+                return response || fetch(evt.request);
+            });
+        })
+    );
+});
